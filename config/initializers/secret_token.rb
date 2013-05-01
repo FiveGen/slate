d935d44b5363d89b13b89e2d0217ccff13c7c91d
Victoria Li <victoli.d@gmail.com>
2013-05-01 07:17:01 -0400

    barebones in palce

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+C::Application.config.secret_key_base = '1ab5dd434747ebbb7ec83a75c3e4472f214e0c4e325579a6386ef3e75a48e70a33051c5eed8483fdad5748bb222bda5be2bf1ab9d8f4bcbd7baf21122ae5dd0c'
